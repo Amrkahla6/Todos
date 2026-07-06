7f53ddfbc453c39bf71f1c8b9d4b1c51e11ecc87
Amr Kahla <amrkahla6@gmail.com>
2026-07-06 08:05:09 +0300

    Create Todo entity

---
+++ src/main/java/com/example/demo/entity/Todo.java
@@ -0,0 +1,86 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "todos")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String priority;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+    public Todo() {
+    }
+
+    public Todo(String title, String description, String priority, boolean complete) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority='" + priority + '\'' +
+                ", complete=" + complete +
+                '}';
+    }
+}
