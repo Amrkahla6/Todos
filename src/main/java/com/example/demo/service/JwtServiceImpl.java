0e7e1311b0a97e13c0a54564d4c0a92c9371fcbb
Amr Kahla <amrkahla6@gmail.com>
2026-07-09 10:14:04 +0300

    Extract claims from JWT

--- src/main/java/com/example/demo/service/JwtServiceImpl.java
+++ src/main/java/com/example/demo/service/JwtServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,9 +9,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService{
@@ -23,7 +24,20 @@ public class JwtServiceImpl implements JwtService{
 
     @Override
     public String extractUserName(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     @Override
